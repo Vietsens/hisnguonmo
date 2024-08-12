800909a42fc77ea24e3cb643b4b393762c0f6536
thinhdt1 <thinhdt1@vietsens.vn>
2024-08-12 14:08:03 +0700

    174277	IVT - Sửa chức năng Thông tin điều trị lao Chỉnh sửa lại không đẩy xml15 nếu đã xóa thông tin

--- HIS/Plugins/HIS.Desktop.Plugins.HisTuberclusisTreatment/frmTuberclusisTreatment.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.HisTuberclusisTreatment/frmTuberclusisTreatment.cs
@@ -477,6 +477,7 @@ namespace HIS.Desktop.Plugins.HisTuberclusisTreatment
             try
             {
                 bool rs = false;
+                
                 CommonParam param = new CommonParam();
                 if (this.btnDel.Enabled == false) return;
                 if (MessageBox.Show(this, "Xóa thông tin điều trị bệnh lao?", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
@@ -485,7 +486,13 @@ namespace HIS.Desktop.Plugins.HisTuberclusisTreatment
                     rs = new BackendAdapter(param).Post<bool>("api/HisTuberculosisTreat/Delete", ApiConsumers.MosConsumer, ID, param);
                     
                     MessageManager.Show(this,param,rs);
-                    
+                    if (rs)
+                    {
+                        foreach (Control control in this.layoutControl3.Controls)
+                        {
+                            control.Text = "";
+                        }
+                    }
                 }
             }
             catch (Exception ex)
