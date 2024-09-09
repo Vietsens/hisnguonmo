57d4a0c6f25601dab622d067bf6855766f945668
Nampp <nampp@vietsens.vn>
2024-09-09 10:01:07 +0700

    175948 IVT - Sửa Thư viện tạo chuỗi QR. Tích hợp thanh toán QR LPBank, Bổ sung thông tin hạn QR

---
+++ Common/Inventec.Common/Inventec.Common.BankQrCode/Provider/LPBANK/Model/LpInfoData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventec.Common.BankQrCode.Provider.LPBANK.Model
+{
+    public class LpInfoData
+    {
+        public string payLoad { get; set; }
+        public string methodCode { get; set; }
+        public string guid { get; set; }
+        public string acquierOrBnb { get; set; }
+        public string merchantOrConsumer { get; set; }
+        public string qrType { get; set; }
+        public string ccy { get; set; }
+        public string CounttryCode { get; set; }
+    }
+}
