70b6d6b0fd89e77f8225ade7bff80672856bd5a6
Nampp <nampp@vietsens.vn>
2024-10-28 08:36:14 +0700

    180003 IVT - Sửa chức năng Chỉ định dịch vụ, thư viện in phiếu chỉ định, thư viện tạo ảnh QR. chức năng Tạo QR Tích hợp thanh toán QR MB và Vietcom

--- HIS/HIS.Desktop.ADO/TransReqQRADO.cs
+++ HIS/HIS.Desktop.ADO/TransReqQRADO.cs
@@ -33,6 +33,7 @@ namespace HIS.Desktop.ADO
         public RefeshReference DelegtePrint { get; set; }
         public V_HIS_DEPOSIT_REQ DepositReq { get; set; }
         public HIS_TRANSACTION Transaction { get; set; }
+        public List<HIS_TRANSACTION> Transactions { get; set; }
     }
     public enum CreateReqType
     {
