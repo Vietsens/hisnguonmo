7be0116dba6db54be692cad1695d9df9fda05113
khain <khainq@vietsens.vn>
2024-11-28 18:02:17 +0700

    181940: IVT - Bổ sung chức năng Nhóm đối tượng HIV/AIDS. Sửa đổi dữ liệu đầu ra XML theo quyết định 3176/QĐ-BYT

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisHivGroupPatient/HisHivGroupPatientUriStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisHivGroupPatient
+{
+    class HisHivGroupPatientUriStore
+    {
+        internal const string HIS_ICD_GROUP_CREATE = "api/HisIcdGroup/Create";
+        internal const string HIS_ICD_GROUP_DELETE = "api/HisIcdGroup/Delete";
+        internal const string HIS_ICD_GROUP_UPDATE = "api/HisIcdGroup/Update";
+        internal const string HIS_ICD_GROUP_GET = "api/HisIcdGroup/Get";
+        internal const string HIS_ICD_GROUP_CHANGE_LOCK = "api/HisIcdGroup/ChangeLock";
+    }
+}
