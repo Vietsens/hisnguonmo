5699b1a1ff65686218b7bce3ebb9db721bffd18a
Nampp <nampp@vietsens.vn>
2025-01-10 10:07:12 +0700

    185570 IVT - Bổ sung chức năng Màn hình chờ phòng mổ 1. Sửa lại giao diện màn hình chờ phòng mổ

--- HIS/HIS.Desktop.ADO/ServiceReqSttSDO.cs
+++ HIS/HIS.Desktop.ADO/ServiceReqSttSDO.cs
@@ -17,6 +17,7 @@
  */
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,5 +27,7 @@ namespace HIS.Desktop.ADO
     public class ServiceReqSttSDO : MOS.EFMODEL.DataModels.HIS_SERVICE_REQ_STT
     {
         public bool checkStt { get; set; }
+        public string DISPLAY_NAME { get; set; }
+        public Color BackColor { get; set; }
     }
 }
