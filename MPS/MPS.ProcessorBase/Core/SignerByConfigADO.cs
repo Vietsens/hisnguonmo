ef0cffdb65c3533a1735aaf196bbe9e660fd1e94
ninhdd <ninhdd@vietsens.vn>
2025-03-10 12:52:21 +0700

    4075 - Sửa thư viện in tự động tạo danh sách người ký.

---
+++ MPS/MPS.ProcessorBase/Core/SignerByConfigADO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPS.ProcessorBase.Core
+{
+    internal class SignerByConfigADO
+    {
+        public string Key { get; set; }
+        public long Num { get; set; }
+    }
+}
