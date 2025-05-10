444a976a27a8f27ef8580a2080c51f3cbab3fcf1
Tran Dang <tranhaidang01082002@gmail.com>
2025-05-10 08:57:46 +0700

    7454 - Bổ sung chức năng "Duyệt khám chuyên khoa"

---
+++ HIS/Plugins/HIS.Desktop.Plugins.ApprovalExamSpecialist/ApprovalExamSpecialist/ApprovalExamSpecialistBehavior.cs
@@ -0,0 +1,61 @@
+﻿using Inventec.Core;
+using HIS.Desktop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventec.Desktop.Core.Actions;
+using Inventec.Desktop.Core;
+using Inventec.Desktop.Core.Tools;
+using System.Windows.Forms;
+using HIS.Desktop.Plugins.ApprovalExamSpecialist.Run;
+
+namespace HIS.Desktop.Plugins.ApprovalExamSpecialist.ApprovalExamSpecialist
+{
+    public sealed class ApprovalExamSpecialistBehavior : Tool<IDesktopToolContext>, IApprovalExamSpecialist
+    {
+        object[] entity;
+        public ApprovalExamSpecialistBehavior()
+            : base()
+        {
+        }
+        public ApprovalExamSpecialistBehavior(CommonParam param, object[] filter)
+            : base()
+        {
+            this.entity = filter;
+        }
+        object IApprovalExamSpecialist.Run()
+        {
+            object result = null;
+            try
+            {
+                Inventec.Desktop.Common.Modules.Module moduleData = null;
+                if (entity != null && entity.Count() > 0)
+                {
+                    for (int i = 0; i < entity.Count(); i++)
+                    {
+                        if (entity[i] is Inventec.Desktop.Common.Modules.Module)
+                        {
+                            moduleData = (Inventec.Desktop.Common.Modules.Module)entity[i];
+                        }
+                    }
+                }
+                if (moduleData != null)
+                {
+                    return new frmApprovalExamSpecialist(moduleData );
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+                result = null;
+            }
+            return result;
+        }
+    }
+}
