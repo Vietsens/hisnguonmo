49256df9a406a13b54b442f8dc26bfe969599b00
thangdq <thangdq@vietsens.vn>
2025-05-27 11:27:20 +0700

    9690 - ae fix lỗi này giúp mình:xml 130 xuất trên his ko lên đc cổng BHYT, phải xuất ra folder rồi đẩy qua VAS mới lên được cổng BHYT

--- HIS/Plugins/HIS.Desktop.Plugins.ExportXmlQD130/frmSettingConfigSync.Designer.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.ExportXmlQD130/frmSettingConfigSync.Designer.cs
@@ -135,6 +135,10 @@ namespace HIS.Desktop.Plugins.ExportXmlQD130
             // 
             this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
             this.bar1});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
             this.barManager1.Form = this;
             this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
             this.bbtnSave});
@@ -163,8 +167,8 @@ namespace HIS.Desktop.Plugins.ExportXmlQD130
             // 
             this.barDockControlTop.CausesValidation = false;
             this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.barDockControlTop.Location = new System.Drawing.Point(0, 29);
-            this.barDockControlTop.Size = new System.Drawing.Size(389, 0);
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Size = new System.Drawing.Size(389, 29);
             // 
             // barDockControlBottom
             // 
