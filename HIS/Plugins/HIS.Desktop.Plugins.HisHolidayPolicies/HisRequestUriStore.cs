24e7ed9b7da6d6f3c4b9690480affed0071cfaf0
tungpd <tungpd@vietsens.vn>
2025-05-30 18:34:19 +0700

    9205: IVT - [Code] - Bổ sung chức năng “thiết lập thời gian ngoài giờ”

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisHolidayPolicies/HisRequestUriStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisHolidayPolicies
+{
+    class HisRequestUriStore
+    {
+        internal const string HIS_DAY_POLICIES_CREATE = "/api/HisHolidayPolicies/Create";
+        internal const string HIS_DAY_POLICIES_GET = "/api/HisHolidayPolicies/Get";
+        internal const string HIS_DAY_POLICIES_UPDATE = "/api/HisHolidayPolicies/Update";
+        internal const string HIS_DAY_POLICIES_DELETE = "/api/HisHolidayPolicies/Delete";
+        internal const string HIS_DAY_POLICIES_CHANGE_LOCK = "/api/HisHolidayPolicies/ChangeLock";
+
+    }
+}
