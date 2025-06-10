9de5bef3c4394597524a405e5b680ffc2c3b637c
Tran Dang <tranhaidang01082002@gmail.com>
2025-06-10 10:15:48 +0700

    việc 10455 + 10554 - IVT - [Code] - Sửa chức năng khu tiếp đón

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisReceptionRoom/Validation/ValidMaxLengthTKCT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisReceptionRoom.Validation
+{
+    class ValidMaxlengthTKCT : DevExpress.XtraEditors.DXErrorProvider.ValidationRule
+    {
+        internal DevExpress.XtraEditors.TextEdit txtTKCT;
+
+        public override bool Validate(System.Windows.Forms.Control control, object value)
+        {
+            bool valid = false;
+            try
+            {
+                if (Inventec.Common.String.CountVi.Count(txtTKCT.Text) > 100)
+                {
+                    this.ErrorText = "Độ dài vượt quá " + 100;
+                    return valid;
+                }
+                else
+                {
+                    valid = true;
+                }
+            }           
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+            }
+            return valid;
+        }
+    }
+}
