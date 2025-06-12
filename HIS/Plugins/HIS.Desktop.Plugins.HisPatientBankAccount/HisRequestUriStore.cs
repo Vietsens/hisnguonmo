3ffb14946cd955dacac9ec3c985c4f99b036789b
huannh <huannh@vietsens.vn>
2025-06-12 19:25:41 +0700

    10458 - IVT - [Code] - Bổ sung chức năng Thông tin thụ hưởng

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisPatientBankAccount/HisRequestUriStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisPatientBankAccount
+{
+    internal class HisRequestUriStore
+    {
+        internal const string CREATE = "api/HisPatientBankAccount/Create";
+        internal const string DELETE = "api/HisPatientBankAccount/Delete";
+        internal const string UPDATE = "api/HisPatientBankAccount/Update";
+        internal const string GET = "api/HisPatientBankAccount/Get";
+        internal const string GETVIEW = "api/HisPatientBankAccount/GetView";
+        internal const string CHANGELOCK = "api/HisPatientBankAccount/ChangeLock";  
+    }
+}
