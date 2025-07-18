0784c701a1509e515f06f02bd07dbaa839a8f39c
Nampp <nampp@vietsens.vn>
2025-07-18 12:57:51 +0700

    11921: Dùng chứng thư số Smart CA của vnpt ký xml đẩy cổng

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisKskDriverList/ADO/HsmConfigADO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisKskDriverList.ADO
+{
+    public class HsmConfigADO
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+        public string DESCRIPTION { get; set; }
+    }
+}
