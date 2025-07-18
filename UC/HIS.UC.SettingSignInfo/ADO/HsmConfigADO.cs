f4f3e96ecf235f21db7019df39952bdfe6524891
Nampp <nampp@vietsens.vn>
2025-07-18 13:32:27 +0700

    11921: Dùng chứng thư số Smart CA của vnpt ký xml đẩy cổng

---
+++ UC/HIS.UC.SettingSignInfo/ADO/HsmConfigADO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.UC.SettingSignInfo.ADO
+{
+    public class HsmConfigADO
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+        public string DESCRIPTION { get; set; }
+    }
+}
