9b78845517515a188d867af295a1944b448f5807
Huynq <huynq1@vietsens.vn>
2025-08-02 09:23:30 +0700

    14723 - IVT - [Code] - Bổ sung chức năng danh sách QR

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisTransReqList/HisTransReqList/frmTransReqList.cs
@@ -0,0 +1,387 @@
+﻿using Inventec.Common.Logging;
+using Inventec.Core;
+using Inventec.Desktop.Common.Message;
+using Inventec.UC.Paging;
+using MOS.EFMODEL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HIS.Desktop.LocalStorage.ConfigApplication;
+using HIS.Desktop.Controls.Session;
+using HIS.Desktop.ApiConsumer;
+using Inventec.Common.Adapter;
+using System.Collections;
+using DevExpress.XtraGrid.Views.Base;
+using System.Security.Cryptography;
+using DevExpress.Data;
+using System.Resources;
+using Inventec.Desktop.Common.LanguageManager;
+using MOS.Filter;
+
+namespace HIS.Desktop.Plugins.HisTransReqList.HisTransReqList
+{
+    public partial class frmTransReqList : HIS.Desktop.Utility.FormBase
+    {
+        int rowCount = 0;
+        int dataTotal = 0;
+        int startPage = 0;
+        int limit = 0;
+        PagingGrid pagingGrid;
+        int positionHandle = -1;
+        V_HIS_TRANS_REQ transReq;
+        Dictionary<string, int> dicOrderTabIndexControl = new Dictionary<string, int>();
+        Inventec.Desktop.Common.Modules.Module moduleData;
+        long? treatment = null;
+        internal long treatmentID;
+        public frmTransReqList(Inventec.Desktop.Common.Modules.Module module, long treatmentID) : base(module)
+        {            
+            try
+            {
+                InitializeComponent();
+                pagingGrid = new PagingGrid();
+                this.moduleData = module;
+
+                this.treatmentID = treatmentID;
+                try
+                {
+                    string iconPath = System.IO.Path.Combine
+                        (HIS.Desktop.LocalStorage.Location.ApplicationStoreLocation.ApplicationStartupPath, System.Configuration.ConfigurationSettings.AppSettings
+                        ["Inventec.Desktop.Icon"]);
+                    this.Icon = Icon.ExtractAssociatedIcon(iconPath);                
+                }
+                catch (Exception ex)
+                {
+                    LogSystem.Warn(ex);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogSystem.Warn(ex);
+            }
+        }
+
+        private void frmTransReqList_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataToControl();
+                SetDefaultFocus();
+                SetResourceByLanguageKey();
+
+                dteTo.EditValue = DateTime.Now;
+                dteFrom.EditValue = DateTime.Now;
+            }
+            catch (Exception ex)
+            {
+                LogSystem.Warn(ex);
+            }
+        }
+
+        private void SetResourceByLanguageKey()
+        {
+            try
+            {
+                Resources.ResourceLanguageManager.LanguageResource = new ResourceManager("HIS.Desktop.Plugins.HisTransReqList.Resources.Lang", typeof
+                (HIS.Desktop.Plugins.HisTransReqList.HisTransReqList.frmTransReqList).Assembly);
+                //layout
+                this.layoutControl1.Text = Inventec.Common.Resource.Get.Value("frmTransReqList.LayoutControl1.Text", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.layoutControl2.Text = Inventec.Common.Resource.Get.Value("frmTransReqList.LayoutControl2.Text", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.btnReset.Text = Inventec.Common.Resource.Get.Value("frmTransReqList.btnReset.Text", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.btnSearch.Text = Inventec.Common.Resource.Get.Value("frmTransReqList.btnSearch.Text", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture()); 
+                //bar button
+                this.bbtnReset.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.bbtnReset.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.bbtnSearch.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.bbtnSearch.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+
+                //column
+                this.gridColumn1.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn1.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn2.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn2.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn3.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn3.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn4.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn4.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn5.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn5.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn6.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn6.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());    
+                this.gridColumn7.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn7.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn8.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn8.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn9.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn9.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn10.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn10.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn11.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn11.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn12.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn12.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn13.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn13.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                this.gridColumn14.Caption = Inventec.Common.Resource.Get.Value("frmTransReqList.gridColumn14.Caption", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+                //input box
+                this.txtSearch.Properties.NullValuePrompt = Inventec.Common.Resource.Get.Value("frmTransReqList.txtSearch.Properties.NullValuePrompt", Resources.ResourceLanguageManager.LanguageResource, LanguageManager.GetCulture());
+
+                if (this.moduleData != null && !String.IsNullOrEmpty(this.moduleData.text))
+                {
+                    this.Text = this.moduleData.text;
+                }
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void SetDefaultFocus()
+        {
+            try
+            {
+                txtSearch.Focus();
+                txtSearch.SelectAll();
+            }
+            catch (Exception ex)
+            {
+                LogSystem.Warn(ex);
+            }
+        }
+
+        private void SetDefaultValue()
+        {
+            try
+            {
+                txtSearch.Text = "";
+                dteFrom.EditValue = DateTime.Now;
+                dteTo.EditValue = DateTime.Now;
+            }
+            catch (Exception ex)
+            {
+                LogSystem.Warn(ex);
+            }
+        }
+
+
+        private void FillDataToControl()
+        {
+            try
+            {
+                WaitingManager.Show();
+                int pageSize = 0;
+                if (ucPaging1.pagingGrid != null)
+                {
+                    pageSize = ucPaging1.pagingGrid.PageSize;
+                }
+                else
+                {
+                    pageSize = ConfigApplicationWorker.Get<int>("CONFIG_KEY__NUM_PAGESIZE");
+                }
+                LoadPaging(new CommonParam(0, pageSize));
+                CommonParam param = new CommonParam();
+                param.Limit = rowCount;
+                param.Count = dataTotal;
+                ucPaging1.Init(LoadPaging, param, pageSize, this.gridControl1);
+                WaitingManager.Hide();
+            }
+            catch(Exception ex) 
+            {
+                LogSystem.Error(ex);
+                WaitingManager.Hide();
+            }
+        }
+
+        private void LoadPaging(Object param)
+        {
+            try
+            {
+                startPage = ((CommonParam)param).Start ?? 0;
+                limit = ((CommonParam)param).Limit ?? 0;
+                CommonParam paramCommon = new CommonParam(startPage, limit);
+                Inventec.Core.ApiResultObject<List<MOS.EFMODEL.DataModels.V_HIS_TRANS_REQ>> apiResult = null;
+                HisTransReqViewFilter filter = new HisTransReqViewFilter();
+                filter.KEY_WORD = txtSearch.Text.Trim();
+                if (dteFrom.EditValue != null && dteFrom.DateTime != DateTime.MinValue)
+                {
+                    filter.CREATE_TIME_FROM = Convert.ToInt64(dteFrom.DateTime.ToString("yyyyMMdd") + "000000");
+                }
+
+                if (dteTo.EditValue != null && dteTo.DateTime != DateTime.MinValue)
+                {
+                    filter.CREATE_TIME_TO = Convert.ToInt64(dteTo.DateTime.ToString("yyyyMMdd") + "235959");
+                }
+                filter.ORDER_DIRECTION = "DESC";
+                filter.ORDER_FIELD = "MODIFY_TIME";
+                filter.TREATMENT_ID = treatmentID;
+                gridView1.BeginDataUpdate();
+                apiResult = new BackendAdapter(paramCommon).GetRO<List<MOS.EFMODEL.DataModels.V_HIS_TRANS_REQ>>(TransReqUriStore.TRANS_REQ_GET,
+                   ApiConsumers.MosConsumer, filter, paramCommon);
+                if (apiResult != null)
+                {
+                    var data = (List<V_HIS_TRANS_REQ>)apiResult.Data;
+                    if (data != null)
+                    {
+                        gridView1.GridControl.DataSource = data;
+                        rowCount = (data == null ? 0 : data.Count);
+                        dataTotal = (apiResult.Param == null ? 0 : apiResult.Param.Count ?? 0);
+                    }
+                }
+                gridView1.EndUpdate();
+                #region Process has exception
+                SessionManager.ProcessTokenLost(paramCommon);
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                LogSystem.Warn(ex);
+            }
+        }   
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataToControl();
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SetDefaultValue();
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void gridView1_CustomRowCellEdit(object sender, DevExpress.XtraGrid.Views.Grid.CustomRowCellEditEventArgs e)
+        {
+            try
+            {
+                DevExpress.XtraGrid.Views.Grid.GridView view = sender as DevExpress.XtraGrid.Views.Grid.GridView;
+                if (e.RowHandle >= 0)
+                {
+                    V_HIS_TRANS_REQ data = (V_HIS_TRANS_REQ)((IList)((BaseView)sender).DataSource)[e.RowHandle];
+                    if (e.Column.FieldName == "LOOK_UP")
+                    {
+                        if (data.TRANS_REQ_STT_ID != IMSys.DbConfig.HIS_RS.HIS_TRANS_REQ_STT.ID__FINISHED &&
+                            data.TRANS_REQ_STT_ID != IMSys.DbConfig.HIS_RS.HIS_TRANS_REQ_STT.ID__FAILED)
+                        {
+                            e.RepositoryItem = btnLookUp;
+                            btnLookUp.ReadOnly = false;
+                            btnLookUp.Enabled = true;
+                        }
+                        else
+                        {
+                            e.RepositoryItem = btnLookUp;
+                            btnLookUp.ReadOnly = true;
+                            btnLookUp.Enabled = false;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void gridView1_CustomUnboundColumnData(object sender, DevExpress.XtraGrid.Views.Base.CustomColumnDataEventArgs e)
+        {
+            try
+            {
+                if (e.IsGetData && e.Column.UnboundType != UnboundColumnType.Bound)
+                {
+                    V_HIS_TRANS_REQ pData = (V_HIS_TRANS_REQ)((IList)((BaseView)sender).DataSource)[e.ListSourceRowIndex];
+                    short status = Inventec.Common.TypeConvert.Parse.ToInt16((pData.IS_ACTIVE ?? -1).ToString());
+                    if (e.Column.FieldName == "STT")
+                    {
+                        e.Value = e.ListSourceRowIndex + 1 + startPage;
+                    }
+                    if(e.Column.FieldName == "TRANS_REQ_STATUS")
+                    {
+                        if (pData.TRANS_REQ_STT_ID == IMSys.DbConfig.HIS_RS.HIS_TRANS_REQ_STT.ID__REQUEST) e.Value = "Yêu cầu";
+                        else if (pData.TRANS_REQ_STT_ID == IMSys.DbConfig.HIS_RS.HIS_TRANS_REQ_STT.ID__FINISHED) e.Value = "Hoàn thành";
+                        else if (pData.TRANS_REQ_STT_ID == IMSys.DbConfig.HIS_RS.HIS_TRANS_REQ_STT.ID__FAILED) e.Value = "Thất bại và kết thúc";
+                        else if (pData.TRANS_REQ_STT_ID == IMSys.DbConfig.HIS_RS.HIS_TRANS_REQ_STT.ID__CANCEL) e.Value = "Hủy";
+                    }
+                    else if (e.Column.FieldName == "CREATE_TIME_STR")
+                    {
+                        try
+                        {
+                            e.Value = Inventec.Common.DateTime.Convert.TimeNumberToTimeString((long)pData.CREATE_TIME);
+                        }
+                        catch (Exception ex)
+                        {
+                            Inventec.Common.Logging.LogSystem.Error(ex);
+                        }
+                    }
+                    else if (e.Column.FieldName == "MODIFY_TIME_STR")
+                    {
+                        try
+                        {
+                            e.Value = Inventec.Common.DateTime.Convert.TimeNumberToTimeString((long)pData.MODIFY_TIME);
+                        }
+                        catch (Exception ex)
+                        {
+                            Inventec.Common.Logging.LogSystem.Error(ex);
+                        }
+                    }
+                }
+                gridControl1.RefreshDataSource();
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void bbtnSearch_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            try
+            {
+                btnSearch_Click(null, null);
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void bbtnReset_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            try
+            {
+                btnReset_Click(null, null);
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void btnLookUp_ButtonClick(object sender, DevExpress.XtraEditors.Controls.ButtonPressedEventArgs e)
+        {
+            CommonParam param = new CommonParam();
+            V_HIS_TRANS_REQ success = new V_HIS_TRANS_REQ();
+            bool notHandler = false;
+            try
+            {
+                V_HIS_TRANS_REQ data = (V_HIS_TRANS_REQ)gridView1.GetFocusedRow();
+                WaitingManager.Show();
+                success = new Inventec.Common.Adapter.BackendAdapter(param).Post<V_HIS_TRANS_REQ>(TransReqUriStore.TRANS_REQ_CHECK, ApiConsumers.MosConsumer, data.ID, param);
+                WaitingManager.Hide();
+                if (success != null)
+                {
+                    notHandler = true;
+                    FillDataToControl();
+                }
+                MessageManager.Show(this, param, notHandler);
+            }
+            catch (Exception ex)
+            {
+                WaitingManager.Hide();
+                Inventec.Common.Logging.LogSystem.Error(ex);
+            }
+        }
+    }
+}
---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisTransReqList/TransReqUriStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisTransReqList
+{
+    class TransReqUriStore
+    {
+        internal const string TRANS_REQ_GET = "api/HisTransReq/Get";
+        internal const string TRANS_REQ_CHECK = "api/HisTransReq/CheckQrStatus";
+    }
+}
