6c50a60dada21594d4d24ab64535c004fc39fb39
Huynq <huynq1@vietsens.vn>
2025-08-14 14:31:15 +0700

    16536: IVT - [Code] - Bổ sung chức năng phác đồ điều trị mẫu

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisRegimenTemp/Validation/ValidationTextEditRegimenTempCode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisRegimenTemp.Validation
+{
+    class ValidationTextEditRegimenTempCode : DevExpress.XtraEditors.DXErrorProvider.ValidationRule
+    {
+        internal DevExpress.XtraEditors.TextEdit txtMa;
+        public override bool Validate(System.Windows.Forms.Control control, object value)
+        {
+            bool valid = false;
+            try
+            {
+                if (string.IsNullOrEmpty(txtMa.Text))
+                {
+                    this.ErrorText = HIS.Desktop.LibraryMessage.MessageUtil.GetMessage(HIS.Desktop.LibraryMessage.Message.Enum.TruongDuLieuBatBuoc);
+                    return valid;
+                }
+                else valid = true;
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+            return valid;
+        }
+    }
+}
