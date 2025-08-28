5c99839971f8dc2894d984aaf71de077a27798d5
thangdq <thangdq@vietsens.vn>
2025-08-28 14:03:34 +0700

    dob

--- HIS/Plugins/EMR.Desktop.Plugins.EmrSignDocumentList/UcEmrSignDocumentList.cs
+++ HIS/Plugins/EMR.Desktop.Plugins.EmrSignDocumentList/UcEmrSignDocumentList.cs
@@ -576,8 +576,8 @@ namespace EMR.Desktop.Plugins.EmrSignDocumentList
                         }
                         else if (e.Column.FieldName == "DOB_STR")
                         {
-                            e.Value = Inventec.Common.DateTime.Convert.TimeNumberToDateString(data.DOB);
-                            if (data.IS_HAS_NOT_DAY_DOB == 1)
+                            e.Value = Inventec.Common.DateTime.Convert.TimeNumberToDateString(data.DOB ?? 0);
+                            if (data.IS_HAS_NOT_DAY_DOB == 1 && data.DOB.HasValue)
                             {
                                 e.Value = data.DOB.ToString().Substring(0, 4);
                             }
