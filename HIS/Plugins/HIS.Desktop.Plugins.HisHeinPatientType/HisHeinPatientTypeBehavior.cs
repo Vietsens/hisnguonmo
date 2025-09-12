0e400a0858d24f1fa2ed8b593ad7042de8328596
khainq <nqkhai1406@gmail.com>
2025-09-12 10:00:30 +0700

    19279 - IVT - [Code] - Bổ sung chức năng mới Đối tượng khám chữa bệnh

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisHeinPatientType/HisHeinPatientTypeBehavior.cs
@@ -0,0 +1,51 @@
+﻿using HIS.Desktop.LocalStorage.BackendData;
+using Inventec.Core;
+using MOS.EFMODEL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisHeinPatientType
+{
+    class HisHeinPatientTypeBehavior : BusinessBase, IHisHeinPatientType
+    {
+        object[] entity;
+        internal HisHeinPatientTypeBehavior(CommonParam param, object[] filter)
+            : base()
+        {
+            this.entity = filter;
+        }
+
+        object IHisHeinPatientType.Run()
+        {
+            try
+            {
+                Inventec.Desktop.Common.Modules.Module moduleData = null;
+
+                if (entity.GetType() == typeof(object[]))
+                {
+                    if (entity != null && entity.Count() > 0)
+                    {
+                        for (int i = 0; i < entity.Count(); i++)
+                        {
+                            if (entity[i] is Inventec.Desktop.Common.Modules.Module)
+                            {
+                                moduleData = (Inventec.Desktop.Common.Modules.Module)entity[i];
+                            }
+                        }
+                    }
+                }
+
+                return new frmHisHeinPatientType(moduleData);
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+                param.HasException = true;
+                return null;
+            }
+        }
+    }
+}
