f78a48bb5fa164f4cab826e5569844886d9a11e4
khainq <nqkhai1406@gmail.com>
2025-09-15 11:44:24 +0700

    dtkcb

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisHeinPatientType/Resources/ResourceLanguageManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisHeinPatientType.Resources
+{
+    class ResourceLanguageManager
+    {
+        public static ResourceManager LanguageResource { get; set; }
+    }
+}
