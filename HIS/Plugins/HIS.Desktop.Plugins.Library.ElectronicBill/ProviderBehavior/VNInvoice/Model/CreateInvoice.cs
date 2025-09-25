af1392411eb6c02e2a6f68364cac08f895f2fdcd
thangdq <thangdq@vietsens.vn>
2025-09-25 13:52:52 +0700

    21052 - IVT - Bổ sung thư viện xuất hóa đơn điện tử VN INVIOCE

---
+++ HIS/Plugins/HIS.Desktop.Plugins.Library.ElectronicBill/ProviderBehavior/VNInvoice/Model/CreateInvoice.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.Library.ElectronicBill.ProviderBehavior.VNInvoice.Model
+{
+    class InputCreateInvoice
+    {
+        public string erpId { get; set; }
+        public string creatorErp { get; set; }
+        public string transactionId { get; set; }
+        public DateTime invoiceDate { get; set; }
+        public string note { get; set; }
+        public string storeCode { get; set; }
+        public string storeName { get; set; }
+        public bool isFinancialLeaseInvoice { get; set; }
+        public string budgetUnitCode { get; set; }
+        public string buyerIDNumber { get; set; }
+        public string paymentMethod { get; set; }
+        public string currency { get; set; }
+        public double exchangeRate { get; set; }
+        public short discountType { get; set; }
+        public decimal totalAmount { get; set; }
+        public decimal totalVatAmount { get; set; }
+        public decimal totalPaymentAmount { get; set; }
+        public decimal totalDiscountAmountBeforeTax { get; set; }
+        public decimal totalDiscountAmountAfterTax { get; set; }
+        public string buyerCode { get; set; }
+        public string buyerEmail { get; set; }
+        public string buyerFullName { get; set; }
+        public string buyerLegalName { get; set; }
+        public string buyerTaxCode { get; set; }
+        public string buyerAddressLine { get; set; }
+        public string buyerDistrictName { get; set; }
+        public string buyerCityName { get; set; }
+        public string buyerCountryCode { get; set; }
+        public string buyerPhoneNumber { get; set; }
+        public string buyerFaxNumber { get; set; }
+        public string buyerBankAccount { get; set; }
+        public string buyerBankName { get; set; }
+        public List<InvoiceDetail> invoiceDetails { get; set; }
+        public List<InvoiceHeaderExtra> invoiceHeaderExtras { get; set; }
+        public List<InvoiceDetailExtra> invoiceDetailExtras { get; set; }
+        public List<InvoiceTaxBreakdown> invoiceTaxBreakdowns { get; set; }
+        public List<InvoiceSpecificProductExtra> invoiceSpecificProductExtras { get; set; }
+    }
+    public class InvoiceSpecificProductExtra
+    {
+        public int type { get; set; }
+        public string fieldName { get; set; }
+        public string fieldValue { get; set; }
+    }
+    public class OutputCreateInvoice
+    {
+        public string id { get; set; }             // id hóa đơn bên VNIs
+        public string erpId { get; set; }          // id hóa đơn bên ERP
+        public string transactionId { get; set; }  // Mã giao dịch
+        public int templateNo { get; set; }        // Mẫu số hóa đơn
+        public string serialNo { get; set; }       // Ký hiệu hóa đơn
+        public string invoiceNo { get; set; }      // Số hóa đơn
+        public int invoiceStatus { get; set; }     // Trạng thái hóa đơn
+        public int signStatus { get; set; }        // Trạng thái ký
+        public bool succeeded { get; set; }
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ HIS/Plugins/HIS.Desktop.Plugins.Library.ElectronicBill/ProviderBehavior/VNInvoice/Model/ReplaceInvoice.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.Library.ElectronicBill.ProviderBehavior.VNInvoice.Model
+{
+    public class InputReplaceInvoice
+    {
+        public string erpIdReference { get; set; }
+        public int templateNo { get; set; }
+        public string serialNo { get; set; }          // Ký hiệu hóa đơn cần thay thế
+        public string invoiceNo { get; set; }         // Số hóa đơn cần thay thế
+        public string erpId { get; set; }             // id hóa đơn bên ERP
+        public string creatorErp { get; set; }        // Người tạo hóa đơn trên ERP
+        public DateTime invoiceDate { get; set; }     // Ngày hóa đơn (yyyy-MM-dd)
+        public string note { get; set; }              // Ghi chú của toàn hóa đơn
+        public string paymentMethod { get; set; }     // Phương thức thanh toán (TM/CK; Tiền mặt; Chuyển khoản…)
+        public string currency { get; set; }          // Tiền tệ (VD: VND)
+        public double exchangeRate { get; set; }
+        public short discountType { get; set; }
+        public decimal totalAmount { get; set; }
+        public decimal totalVatAmount { get; set; }
+        public decimal totalPaymentAmount { get; set; }          
+        public double totalDiscountAmountBeforeTax { get; set; }  
+        public string buyerCode { get; set; }                  
+        public string buyerEmail { get; set; }                   
+        public string buyerFullName { get; set; }                
+        public string buyerLegalName { get; set; }                
+        public string buyerTaxCode { get; set; }
+        public string buyerAddressLine { get; set; }    // Địa chỉ khách hàng
+        public string buyerDistrictName { get; set; }   // Quận/huyện khách hàng
+        public string buyerCityName { get; set; }       // Tỉnh/thành phố khách hàng
+        public string buyerCountryCode { get; set; }    // Mã quốc gia khách hàng
+        public string buyerPhoneNumber { get; set; }    // Số điện thoại khách hàng
+        public string buyerFaxNumber { get; set; }      // Số Fax khách hàng
+        public string buyerBankAccount { get; set; }    // Số tài khoản ngân hàng
+        public string buyerBankName { get; set; }       // Chi nhánh ngân hàng của khách hàng
+        public List<InvoiceDetail> invoiceDetails { get; set; }
+        public List<InvoiceDetailExtra> invoiceDetailExtras { get; set; }
+        public List<InvoiceHeaderExtra> invoiceHeaderExtras { get; set; }
+        public List<InvoiceTaxBreakdown> invoiceTaxBreakdowns { get; set; }
+        public InvoicePrintNote invoicePrintNote { get; set; }
+    }
+    public class InvoiceDetail
+    {
+        public int index { get; set; }                        // Số thứ tự chi tiết trong hóa đơn
+        public decimal discountAmountBeforeTax { get; set; }  // Tiền chiết khấu cho dòng chi tiết (trước thuế)
+        public double discountPercentBeforeTax { get; set; }  // Phần trăm chiết khấu cho dòng chi tiết (trước thuế)
+        public decimal paymentAmount { get; set; }            // Thành tiền sau VAT của dòng chi tiết
+        public string productCode { get; set; }                  // Mã sản phẩm
+        public int productType { get; set; }
+        public string productName { get; set; }                  // Tên sản phẩm
+        public string unitName { get; set; }                     // Đơn vị tính
+        public decimal unitPrice { get; set; }                   // Đơn giá
+        public double quantity { get; set; }                     // Số lượng
+        public decimal amount { get; set; }                      // Thành tiền chưa VAT
+        public int vatPercent { get; set; }                      // Phần trăm thuế
+        public decimal vatAmount { get; set; }
+        public string note { get; set; }
+    }
+    public class InvoiceHeaderExtra
+    {
+        public string fieldName { get; set; }    // Tên trường mở rộng
+        public string fieldValue { get; set; }   // Giá trị trường mở rộng
+    }
+    public class InvoiceDetailExtra
+    {
+        public string fieldName { get; set; }    // Tên trường mở rộng
+        public string fieldValue { get; set; }   // Giá trị trường mở rộng
+    }
+    public class InvoiceTaxBreakdown
+    {
+        public decimal vatAmount { get; set; }
+        public decimal amount { get; set; }
+        public int vatPercent { get; set; }
+    }
+    public class InvoicePrintNote
+    {
+        public string note { get; set; }
+        public bool isShowNote { get; set; }
+    }
+    public class OutputReplaceInvoice
+    {
+        public string id { get; set; }             // id hóa đơn bên VNIs
+        public string erpId { get; set; }          // id hóa đơn bên ERP
+        public string transactionId { get; set; }  // Mã giao dịch
+        public int templateNo { get; set; }        // Mẫu số hóa đơn
+        public string serialNo { get; set; }       // Ký hiệu hóa đơn
+        public string invoiceNo { get; set; }      // Số hóa đơn
+        public int invoiceStatus { get; set; }     // Trạng thái hóa đơn
+        public int signStatus { get; set; }        // Trạng thái ký
+        public bool succeeded { get; set; }       
+        public int code { get; set; }       
+        public string message { get; set; }
+    }
+
+}
+
