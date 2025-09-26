09eee544c6d79d3bae2f57ba08093985654cddb3
huannh <huannh@vietsens.vn>
2025-09-26 17:12:08 +0700

    update ngân hàng thanh toán 2 sổ

---
+++ HIS/Plugins/HIS.Desktop.Plugins.TransactionBillTwoInOne/ADO/PayFormADO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.TransactionBillTwoInOne.ADO
+{
+        internal class PayFormADO : MOS.EFMODEL.DataModels.HIS_PAY_FORM
+        {
+            public string PayFormId { get; set; }
+            public long? BANK_ID { get; set; }
+        }
+    
+}
