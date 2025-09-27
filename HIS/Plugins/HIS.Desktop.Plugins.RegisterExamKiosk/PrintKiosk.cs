8fe0be0527370a0797aad8906d966b65ab14f6cc
phuongnm <minhphuongn872@gmail.com>
2025-09-27 14:22:27 +0700

    21494 - Sửa Mps000025- thêm QR thanh toán

--- HIS/Plugins/HIS.Desktop.Plugins.RegisterExamKiosk/PrintKiosk.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.RegisterExamKiosk/PrintKiosk.cs
@@ -1,4 +1,4 @@
-/* IVT
+﻿/* IVT
  * @Project : hisnguonmo
  * Copyright (C) 2017 INVENTEC
  *  
@@ -17,12 +17,16 @@
  */
 using HIS.Desktop.ApiConsumer;
 using HIS.Desktop.Common;
+using HIS.Desktop.LocalStorage.BackendData;
 using HIS.Desktop.LocalStorage.LocalData;
+using HIS.Desktop.Plugins.RegisterExamKiosk.Config;
 using Inventec.Common.Adapter;
 using Inventec.Common.Logging;
 using Inventec.Core;
 using Inventec.Desktop.Common.Message;
+using LibUsbDotNetF;
 using MOS.EFMODEL.DataModels;
+using MOS.Filter;
 using MOS.SDO;
 using MPS.ProcessorBase.Core;
 using System;
@@ -49,6 +53,8 @@ namespace HIS.Desktop.Plugins.RegisterExamKiosk
         List<V_HIS_TRANSACTION> ListTransaction { get; set; }
         bool checkPrintAgain { get; set; }
         List<HIS_CARD> listHisCard { get; set; }
+        List<HIS_TRANS_REQ> TransReq { get; set; }
+
 
         public PrintKiosk(V_HIS_PATIENT_TYPE_ALTER patyAlter, V_HIS_SERVICE_REQ ServiceReqPrint, List<V_HIS_SERE_SERV> sereServs, DelegateReturnSuccess success, string printTypeCode, string fileName, HIS_TREATMENT treatmentPrint, List<HIS_SERE_SERV_DEPOSIT> _ListSereServDeposit, List<HIS_SERE_SERV_BILL> _ListSereServBill, List<V_HIS_TRANSACTION> _ListTransaction, bool _checkPrintAgain)
         {
@@ -83,7 +89,55 @@ namespace HIS.Desktop.Plugins.RegisterExamKiosk
         {
             try
             {
-                MPS.Processor.Mps000025.PDO.Mps000025PDO mps000025PDO = new MPS.Processor.Mps000025.PDO.Mps000025PDO(this.ListSereServDeposit, this.ListSereServBill, this.ListTransaction, this.ServiceReqPrint, this.sereServs, this.patyAlter, this.patyAlter.PATIENT_TYPE_NAME, treatmentPrint, this.listHisCard);
+                List<HIS_TRANS_REQ> transReq = null;
+                HIS_TRANS_REQ transReqObj = null;
+
+                if (AppConfigs.HisTranReqQRCodeTreatmentPrint)
+                {
+                    if (this.ServiceReqPrint != null)
+                    {
+                        CommonParam param = new CommonParam();
+                        HisTransReqFilter filter = new HisTransReqFilter();
+                        filter.IDs = new List<long> { this.ServiceReqPrint.TRANS_REQ_ID ?? 0 };
+                        transReq = new Inventec.Common.Adapter.BackendAdapter(param)
+                            .Get<List<HIS_TRANS_REQ>>("api/HisTransReq/Get", ApiConsumers.MosConsumer, filter, param);
+                    }
+                }
+                else
+                {
+                    if(this.ServiceReqPrint != null)
+                    {
+                        CommonParam param = new CommonParam();
+                        HisTransReqFilter filter = new HisTransReqFilter();
+                        filter.TREATMENT_ID = this.ServiceReqPrint.TREATMENT_ID;
+                        transReq = new Inventec.Common.Adapter.BackendAdapter(param)
+                            .Get<List<HIS_TRANS_REQ>>("api/HisTransReq/Get", ApiConsumers.MosConsumer, filter, param);
+                        if (transReq != null && transReq.Count > 0)
+                        {
+                            transReq = transReq.Where(o => o.TRANS_REQ_TYPE == IMSys.DbConfig.HIS_RS.HIS_TRANS_REQ_TYPE.ID__BY_SERVICE).ToList();
+                        }
+                    }
+                }
+                var lstConfig = BackendDataWorker.Get<HIS_CONFIG>().Where(o => o.KEY.StartsWith("HIS.Desktop.Plugins.PaymentQrCode") && !string.IsNullOrEmpty(o.VALUE)).ToList();
+
+                if (transReq != null && transReq.Count > 0)
+                {
+                    transReqObj = AppConfigs.HisTranReqQRCodeTreatmentPrint
+                        ? transReq.FirstOrDefault(o => o.ID == ServiceReqPrint.TRANS_REQ_ID)
+                        : transReq.OrderByDescending(o => o.CREATE_TIME).FirstOrDefault();
+                }
+                MPS.Processor.Mps000025.PDO.Mps000025PDO mps000025PDO = new MPS.Processor.Mps000025.PDO.Mps000025PDO(
+                    this.ListSereServDeposit,
+                    this.ListSereServBill,
+                    this.ListTransaction,
+                    this.ServiceReqPrint,
+                    this.sereServs,
+                    transReqObj,
+                    lstConfig,
+                    this.patyAlter,
+                    this.patyAlter.PATIENT_TYPE_NAME, 
+                    treatmentPrint
+                );
                 LogSystem.Debug(LogUtil.TraceData("Du lieu mps000025PDO khi dang ky kham", mps000025PDO));
                 WaitingManager.Hide();
 
@@ -158,10 +212,59 @@ namespace HIS.Desktop.Plugins.RegisterExamKiosk
         {
             try
             {
-                MPS.Processor.Mps000025.PDO.Mps000025PDO mps000025PDO = new MPS.Processor.Mps000025.PDO.Mps000025PDO(this.ListSereServDeposit, this.ListSereServBill, this.ListTransaction, this.ServiceReqPrint, this.sereServs, this.patyAlter, this.patyAlter.PATIENT_TYPE_NAME, treatmentPrint);
+                List<HIS_TRANS_REQ> transReq = null;
+                HIS_TRANS_REQ transReqMPS37 = null;
+                if (AppConfigs.HisTranReqQRCodeTreatmentPrint)
+                {
+                    if (this.ServiceReqPrint != null)
+                    {
+                        CommonParam param = new CommonParam();
+                        HisTransReqFilter filter = new HisTransReqFilter();
+                        filter.IDs = new List<long> { this.ServiceReqPrint.TRANS_REQ_ID ?? 0 };
+                        transReq = new Inventec.Common.Adapter.BackendAdapter(param)
+                            .Get<List<HIS_TRANS_REQ>>("api/HisTransReq/Get", ApiConsumers.MosConsumer, filter, param);
+                    }
+                }
+                else
+                {
+                    if (this.ServiceReqPrint != null)
+                    {
+                        CommonParam param = new CommonParam();
+                        HisTransReqFilter filter = new HisTransReqFilter();
+                        filter.TREATMENT_ID = this.ServiceReqPrint.TREATMENT_ID;
+                        transReq = new Inventec.Common.Adapter.BackendAdapter(param)
+                            .Get<List<HIS_TRANS_REQ>>("api/HisTransReq/Get", ApiConsumers.MosConsumer, filter, param);
+                        if (transReq != null && transReq.Count > 0)
+                        {
+                            transReq = transReq.Where(o => o.TRANS_REQ_TYPE == IMSys.DbConfig.HIS_RS.HIS_TRANS_REQ_TYPE.ID__BY_SERVICE).ToList();
+                            transReqMPS37 = transReq.OrderByDescending(o => o.CREATE_TIME).FirstOrDefault();
+                        }
+                    }
+                }
+                HIS_TRANS_REQ transReqObj = null;
+                if (transReq != null && transReq.Count > 0)
+                {
+                    transReqObj = AppConfigs.HisTranReqQRCodeTreatmentPrint
+                        ? transReq.FirstOrDefault(o => o.ID == ServiceReqPrint.TRANS_REQ_ID)
+                        : transReq.OrderByDescending(o => o.CREATE_TIME).FirstOrDefault();
+                }
+               var lstConfig = BackendDataWorker.Get<HIS_CONFIG>().Where(o => o.KEY.StartsWith("HIS.Desktop.Plugins.PaymentQrCode") && !string.IsNullOrEmpty(o.VALUE)).ToList();
+
+
+                MPS.Processor.Mps000025.PDO.Mps000025PDO mps000025PDO = new MPS.Processor.Mps000025.PDO.Mps000025PDO(
+                     this.ListSereServDeposit,
+                     this.ListSereServBill,
+                     this.ListTransaction,
+                     this.ServiceReqPrint,
+                     this.sereServs,
+                     transReqObj,
+                     lstConfig,
+                     this.patyAlter,
+                     this.patyAlter.PATIENT_TYPE_NAME, 
+                     treatmentPrint
+                );
                 LogSystem.Debug(LogUtil.TraceData("Du lieu mps000025PDO khi dang ky kham", mps000025PDO));
                 WaitingManager.Hide();
-
                 string templateFile = Application.StartupPath + "\\Mps000025__Temp__" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".xlsx";
                 if (File.Exists(templateFile))
                 {
@@ -171,7 +274,6 @@ namespace HIS.Desktop.Plugins.RegisterExamKiosk
                     }
                     catch { }
                 }
-
                 LogSystem.Info(LogUtil.TraceData("Du lieu templateFile truyen vao", templateFile));
 
                 PrintData printData = new PrintData(printTypeCode, fileName, mps000025PDO, MPS.ProcessorBase.PrintConfig.PreviewType.SaveFile, "", 1, templateFile);
