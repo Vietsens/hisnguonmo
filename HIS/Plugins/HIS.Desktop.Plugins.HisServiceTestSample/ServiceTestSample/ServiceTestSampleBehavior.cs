2dffd2762a66a620d6cacee08321e2bfd63cbdbe
khainq <nqkhai1406@gmail.com>
2025-11-18 17:41:19 +0700

    Việc 27853: IVT- [Code] - Bổ sung chức năng mới

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisServiceTestSample/ServiceTestSample/ServiceTestSampleBehavior.cs
@@ -0,0 +1,68 @@
+﻿using Inventec.Core;
+using Inventec.Desktop.Core;
+using Inventec.Desktop.Core.Tools;
+using MOS.EFMODEL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisServiceTestSample.ServiceTestSample
+{
+    class ServiceTestSampleBehavior : Tool<IDesktopToolContext>, IServiceTestSample
+    {
+        object[] entity;
+        long treatmentId;
+        Inventec.Desktop.Common.Modules.Module currentModule;
+        V_HIS_SERVICE service;
+
+        internal ServiceTestSampleBehavior()
+            : base()
+        {
+
+        }
+
+        internal ServiceTestSampleBehavior(CommonParam param, object[] data)
+            : base()
+        {
+            entity = data;
+        }
+
+        object IServiceTestSample.Run()
+        {
+            object result = null;
+            try
+            {
+                if (entity != null && entity.Count() > 0)
+                {
+                    foreach (var item in entity)
+                    {
+                        if (item is V_HIS_SERVICE)
+                        {
+                            service = (V_HIS_SERVICE)item;
+                        }
+                        if (item is Inventec.Desktop.Common.Modules.Module)
+                        {
+                            this.currentModule = (Inventec.Desktop.Common.Modules.Module)item;
+                        }
+                    }
+                    if (service != null)
+                    {
+                        result = new UCServiceTestSample(service, this.currentModule);
+                    }
+                    else
+                    {
+                        result = new UCServiceTestSample();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+                result = null;
+            }
+            return result;
+        }
+    }
+}
---
+++ UC/HIS.UC.TestSample/UCTestSampleProcessor.cs
@@ -0,0 +1,73 @@
+﻿using HIS.UC.TestSample.ADO;
+using HIS.UC.TestSample.GetDataGridView;
+using HIS.UC.TestSample.Reload;
+using HIS.UC.TestSample.Run;
+using HIS.UC.TestSample.Run;
+using Inventec.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HIS.UC.TestSample
+{
+    public class UCTestSampleProcessor : BussinessBase
+    {
+        object uc;
+        public UCTestSampleProcessor()
+            : base()
+        {
+        }
+
+        public UCTestSampleProcessor(CommonParam paramBusiness)
+            : base(paramBusiness)
+        {
+        }
+
+        public object Run(TestSampleInitADO arg)
+        {
+            uc = null;
+            try
+            {
+                IRun behavior = RunFactory.MakeIExpMestMedicineGrid(param, arg);
+                uc = behavior != null ? (behavior.Run()) : null;
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+                uc = null;
+            }
+            return uc;
+        }
+
+        public void Reload(UserControl control, List<TestSampleADO> data)   
+        {
+            try
+            {
+                IReload behavior = ReloadFactory.MakeIReload(param, (control == null ? (UserControl)uc : control), data);
+                if (behavior != null) behavior.Run();
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+            }
+        }
+
+        public object GetDataGridView(UserControl control)
+        {
+            object result = null;
+            try
+            {
+                IGetDataGridView behavior = GetDataGridViewFactory.MakeIGetDataGridView(param, (control == null ? (UserControl)uc : control));
+                result = (behavior != null) ? behavior.Run() : null;
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+            }
+            return result;
+        }
+    }
+}
