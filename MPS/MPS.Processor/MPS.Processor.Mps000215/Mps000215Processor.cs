06c30b9c6076aa450b00eaf31d2ce4d4420360db
thangdq <thangdq@vietsens.vn>
2025-11-24 15:57:11 +0700

    27844 - IVT - [Code] - Sửa biểu in In phiếu xuất bù cơ số TT (Mps000215)

--- MPS/MPS.Processor/MPS.Processor.Mps000215/Mps000215Processor.cs
+++ MPS/MPS.Processor/MPS.Processor.Mps000215/Mps000215Processor.cs
@@ -767,9 +767,9 @@ namespace MPS.Processor.Mps000215
                     string parentName = (first.MEDICINE_PARENT_NAME ?? "").Trim();
 
                     if (string.IsNullOrWhiteSpace(parentName))
-                        parentName = "PHIẾU BÙ VẬT TƯ";
+                        parentName = "";
                     else
-                        parentName = "PHIẾU BÙ VẬT TƯ " + parentName.ToUpper();
+                        parentName = parentName.ToUpper();
 
                     first.MEDICINE_PARENT_NAME = parentName;
 
