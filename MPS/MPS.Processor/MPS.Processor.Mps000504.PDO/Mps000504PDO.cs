f64ac60ef209e3e9dfb1c017ecacec04d4b42f6f
Huynq <huynq1@vietsens.vn>
2025-12-02 13:54:43 +0700

    29417 - IVT - [Code] - Bổ sung Mps000504 Bảng kê chi phí theo điều kiện lọc

---
+++ MPS/MPS.Processor/MPS.Processor.Mps000504.PDO/Mps000504PDO.cs
@@ -0,0 +1,25 @@
+﻿using MOS.EFMODEL.DataModels;
+using MPS.ProcessorBase.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPS.Processor.Mps000504.PDO
+{
+    public class Mps000504PDO : RDOBase
+    {
+        public V_HIS_TREATMENT_FEE Treatment { get; set; }
+        public List<V_HIS_SERE_SERV> HisSereServ { get; set; }
+        public long fromDateReq { get; set; }
+        public long toDateReq { get; set; }
+        public Mps000504PDO(V_HIS_TREATMENT_FEE treatment, List<V_HIS_SERE_SERV> hisSereServ, long fromDateReq, long toDateReq)
+        {
+            this.HisSereServ = hisSereServ;
+            this.Treatment = treatment;
+            this.fromDateReq = fromDateReq;
+            this.toDateReq = toDateReq;
+        }
+    }
+}
---
+++ MPS/MPS.Processor/MPS.Processor.Mps000504/Mps000504ExtendSingleKey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPS.Processor.Mps000504
+{
+    class Mps000504ExtendSingleKey
+    {
+        internal const string STR_HEIN_CARD_FROM_TIME = "STR_HEIN_CARD_FROM_TIME";
+        internal const string STR_HEIN_CARD_TO_TIME = "STR_HEIN_CARD_TO_TIME";
+        internal const string STR_HEIN_CARD_NUMBER = "STR_HEIN_CARD_NUMBER";
+        internal const string GENDER_NAME = "GENDER_NAME";
+
+    }
+}
