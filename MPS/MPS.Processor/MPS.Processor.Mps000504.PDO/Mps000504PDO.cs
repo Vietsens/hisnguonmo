499b5f93a2d582c8207ff57ba1483be6bdd6f9a0
Huynq <huynq1@vietsens.vn>
2025-12-05 11:15:46 +0700

    29417 - IVT - [Code] - Bổ sung Mps000504 Bảng kê chi phí theo điều kiện lọc

--- MPS/MPS.Processor/MPS.Processor.Mps000504.PDO/Mps000504PDO.cs
+++ MPS/MPS.Processor/MPS.Processor.Mps000504.PDO/Mps000504PDO.cs
@@ -12,8 +12,8 @@ namespace MPS.Processor.Mps000504.PDO
     {
         public V_HIS_TREATMENT_FEE Treatment { get; set; }
         public List<V_HIS_SERE_SERV> HisSereServ { get; set; }
-        public long fromDateReq { get; set; }
-        public long toDateReq { get; set; }
+        public long? fromDateReq { get; set; }
+        public long? toDateReq { get; set; }
         public Mps000504PDO(V_HIS_TREATMENT_FEE treatment, List<V_HIS_SERE_SERV> hisSereServ, long fromDateReq, long toDateReq)
         {
             this.HisSereServ = hisSereServ;
