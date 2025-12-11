ca075fa771d649de711d9034dde1bf2b45bab80f
huannh <huannh@vietsens.vn>
2025-12-11 15:45:08 +0700

    30225 - IVT - [Code] - Bổ sung thư viện dùng chung kết nối đến hệ thống lưu trữ thông tin của 2ID

---
+++ HIS/Plugins/HIS.Desktop.Plugins.Library.TwoIDStorageIntegration/StorageApiClient.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.Library.TwoIDStorageIntegration
+{
+    internal class StorageApiClient
+    {
+        public static T CreateRequest<T>(string baseUri, string requestUri, object sendData, string contentType)
+        {
+            using (var client = new HttpClient())
+            {
+              
+                if (!baseUri.EndsWith("/"))
+                    baseUri += "/";
+
+            
+                if (requestUri.StartsWith("/"))
+                    requestUri = requestUri.Substring(1);
+
+                string fullUrl = baseUri + requestUri; 
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.Timeout = new TimeSpan(0, 0, 90);
+
+                string sendJsonData = JsonConvert.SerializeObject(sendData);
+                Inventec.Common.Logging.LogSystem.Info("_____sendJsonData : " + sendJsonData);
+
+                HttpResponseMessage resp = client.PostAsync(fullUrl, new StringContent(sendJsonData, Encoding.UTF8, contentType)).Result;
+
+                if (resp == null || !resp.IsSuccessStatusCode)
+                {
+                    int statusCode = resp == null ? 0 : (int)resp.StatusCode;
+                    throw new Exception(string.Format("Lỗi khi gọi API: {0}. StatusCode: {1}", fullUrl, statusCode));
+                }
+
+                string responseData = resp.Content.ReadAsStringAsync().Result;
+                Inventec.Common.Logging.LogSystem.Info("api responseData: " + responseData);
+
+                T data = JsonConvert.DeserializeObject<T>(responseData);
+                if (data == null)
+                {
+                    throw new Exception(string.Format("Lỗi khi gọi API. Response {0}:", responseData));
+                }
+                return data;
+            }
+        }
+    }
+}
