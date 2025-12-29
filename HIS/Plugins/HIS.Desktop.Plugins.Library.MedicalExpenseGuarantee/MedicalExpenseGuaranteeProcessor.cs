8a667bb9569e8e15f570f206a50ebf11002ad036
khainq <khainq@vietsens.vn>
2025-12-29 16:09:30 +0700

    Tài liệu 2152: Chức năng bảo lãnh viện phí

---
+++ HIS/Plugins/HIS.Desktop.Plugins.Library.MedicalExpenseGuarantee/MedicalExpenseGuaranteeProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.Library.MedicalExpenseGuarantee
+{
+    public class MedicalExpenseGuaranteeProcessor
+    {
+        string address {  get; set; }
+        string code {  get; set; }
+        string limit {  get; set; }
+
+        public MedicalExpenseGuaranteeProcessor(string address, string code, string limit)
+        {
+            this.address = address;
+            this.code = code;
+            this.limit = limit;
+        }
+    }
+}
