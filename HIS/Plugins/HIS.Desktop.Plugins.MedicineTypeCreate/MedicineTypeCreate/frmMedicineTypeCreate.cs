a4847b935a5985fbeecb0a2f1c2f1048931e86fc
Huynq <huynq1@vietsens.vn>
2026-01-19 08:46:03 +0700

    2

--- HIS/Plugins/HIS.Desktop.Plugins.MedicineTypeCreate/MedicineTypeCreate/frmMedicineTypeCreate.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.MedicineTypeCreate/MedicineTypeCreate/frmMedicineTypeCreate.cs
@@ -1206,7 +1206,7 @@ namespace HIS.Desktop.Plugins.MedicineTypeCreate.MedicineTypeCreate
                 var arr = new List<string>();
 
                 if (hIS_MEDICINE_TYPE.IS_VACCINE == 1)
-                    arr.Add("Vaccin");
+                    arr.Add("Vaccine");
 
                 if (hIS_MEDICINE_TYPE.IS_CHEMICAL_SUBSTANCE == 1)
                     arr.Add("Hóa chất");
@@ -1218,13 +1218,13 @@ namespace HIS.Desktop.Plugins.MedicineTypeCreate.MedicineTypeCreate
                     arr.Add("Generic");
 
                 if (hIS_MEDICINE_TYPE.IS_FUNCTIONAL_FOOD == 1)
-                    arr.Add("TPCN");
+                    arr.Add("Thực phẩm chức năng");
 
                 if (hIS_MEDICINE_TYPE.IS_VITAMIN_A == 1)
                     arr.Add("Vitamin A");
 
                 if (hIS_MEDICINE_TYPE.IS_TCMR == 1)
-                    arr.Add("TCMR");
+                    arr.Add("Tiêm chủng mở rộng");
 
                 if (hIS_MEDICINE_TYPE.IS_BIOLOGIC == 1)
                     arr.Add("Sinh phẩm");
@@ -1236,13 +1236,13 @@ namespace HIS.Desktop.Plugins.MedicineTypeCreate.MedicineTypeCreate
                     arr.Add("Biệt dược gốc");
 
                 if (hIS_MEDICINE_TYPE.IS_STAR_MARK == 1)
-                    arr.Add("Thuốc dấu *");
+                    arr.Add("Thuốc dấu sao *");
 
                 if (hIS_MEDICINE_TYPE.IS_KIDNEY == 1)
                     arr.Add("Thuốc chạy thận");
 
                 if (hIS_MEDICINE_TYPE.IS_RAW_MEDICINE == 1)
-                    arr.Add("NLBC");
+                    arr.Add("Nguyên liệu điều chế");
 
                 string listArrayMedicine = string.Join(", ", arr);
 
