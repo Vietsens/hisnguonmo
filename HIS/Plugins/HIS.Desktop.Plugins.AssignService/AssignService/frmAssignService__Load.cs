80d0486063814a1b7a5b7562815b9b013691d523
thangdq <thangdq@vietsens.vn>
2026-02-09 15:43:18 +0700

    36153 - IVT - [Code] - Sửa chức năng chỉ định(HIS.Desktop.Plugins.AssignService) (bảo lãnh)

--- HIS/Plugins/HIS.Desktop.Plugins.AssignService/AssignService/frmAssignService__Load.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.AssignService/AssignService/frmAssignService__Load.cs
@@ -4140,7 +4140,7 @@ namespace HIS.Desktop.Plugins.AssignService.AssignService
             try
             {
                 // Nếu không có thông tin bảo lãnh hoặc không có GUARANTEE_CODE thì bỏ qua
-                if (this.guaranteeInfo == null || (this.currentHisTreatment != null && string.IsNullOrEmpty(this.currentHisTreatment.GUARANTEE_CODE)))
+                if (this.guaranteeInfo == null || (this.currentHisTreatment != null && string.IsNullOrEmpty(this.currentHisTreatment.GUARANTEE_CODE)) || this.currentTreatment.TDL_PATIENT_TYPE_ID == HisConfigCFG.PatientTypeId__BHYT)
                     return true;
                 //CommonParam param = new CommonParam();
                 //HisSereServBillFilter  hisSereServBillFilter = new HisSereServBillFilter();
@@ -4154,7 +4154,7 @@ namespace HIS.Desktop.Plugins.AssignService.AssignService
                 Inventec.Common.Logging.LogSystem.Debug("totalGuaranteeArise qtcode: " + Inventec.Common.Logging.LogUtil.TraceData("Data", totalGuaranteeArise));
                 if ((this.totalGuaranteePrice_1 - this.treatmentPrint.TOTAL_BILL_AMOUNT) > this.guaranteeInfo.GUARANTEE_BALANCE)
                 {
-                    message = "Tổng tiền dịch vụ đã vượt hạn mức bảo lãnh, vui lòng kiểm tra lại.";
+                    message = "Tổng tiền dịch vụ đã vượt hạn mức, vui lòng kiểm tra lại.";
                     return false;
                 }
 
