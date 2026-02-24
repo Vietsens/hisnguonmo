46913fc9782fcefae19b4e50207b43f00156b117
thangdq <thangdq@vietsens.vn>
2026-02-24 16:14:15 +0700

    37039 - IVT - [Code] - Chức năng Viện phí(thêm btn chốt nợ, thu nợ)

--- HIS/Plugins/HIS.Desktop.Plugins.Transaction/Config/HisConfigCFG.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.Transaction/Config/HisConfigCFG.cs
@@ -42,8 +42,10 @@ namespace HIS.Desktop.Plugins.Transaction.Config
         private const string CONFIG_KEY__DIRECTLY_BILLING_OPTION = "MOS.HIS_TRANSACTION.DIRECTLY_BILLING_OPTION";
         private const string CONFIG_KEY__UNLOCK_FEE_OPTION = "HIS.DESKTOP.HIS_TREATMENT.UNLOCK_FEE_OPTION";
         private const string CONFIG_KEY__CALL_PATIENT_FORMAT = "HIS.TRANSACTION_ROOM.CALL_PATIENT_FORMAT";
+        private const string CONFIG_KEY__SHOW_BTN_TRANSACTION_DEBT = "HIS.Desktop.Plugins.Transaction.ShowBtnTransactionDebt";
 
         internal static string CallPatientFormat;
+        internal static string ShowBtnTransactionDebt;
         internal static string PatientTypeCode__BHYT;
         internal static long PatientTypeId__BHYT;
 
@@ -80,6 +82,7 @@ namespace HIS.Desktop.Plugins.Transaction.Config
                 LogSystem.Debug("LoadConfig => 1");
                 CallPatientFormat = GetValue(CONFIG_KEY__CALL_PATIENT_FORMAT);
                 IsNotBillCFG = GetValue(IsNotBillString);
+                ShowBtnTransactionDebt = GetValue(CONFIG_KEY__SHOW_BTN_TRANSACTION_DEBT); 
                 ShowElectronicNumorderCFG = GetValue(ShowElectronicNumorderString);
                 DirectlyBillingOption = GetValue(CONFIG_KEY__DIRECTLY_BILLING_OPTION);
                 IsAllowAfterLocking = GetValue(CONFIG_KEY__ALLOWAFTERLOCKING) == "1";
