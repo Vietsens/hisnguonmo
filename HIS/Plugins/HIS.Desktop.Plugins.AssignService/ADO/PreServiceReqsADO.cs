687db6a0ad02f17cfce4901c279457ed456c6c2b
thangdq <thangdq@vietsens.vn>
2026-03-02 15:22:17 +0700

    đẩy ado của chỉ định

---
+++ HIS/Plugins/HIS.Desktop.Plugins.AssignService/ADO/PreServiceReqsADO.cs
@@ -0,0 +1,21 @@
+﻿using MOS.EFMODEL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.AssignService.ADO
+{
+    class PreServiceReqsADO : V_HIS_SERVICE_REQ_6
+    {
+        public bool IsReqPicked { get; set; } = false;  // Trạng thái checkbox
+
+    
+        public PreServiceReqsADO(V_HIS_SERVICE_REQ_6 source)
+        {
+            if (source == null) return;
+            Inventec.Common.Mapper.DataObjectMapper.Map<PreServiceReqsADO>(this, source);
+        }
+    }
+}
