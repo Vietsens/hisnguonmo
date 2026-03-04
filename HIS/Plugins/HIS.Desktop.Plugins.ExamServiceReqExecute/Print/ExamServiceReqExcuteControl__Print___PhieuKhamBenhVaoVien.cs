26883dd5209fbfab79863f41d815474098e79041
hungnq <ngoquanghunga6@gmail.com>
2026-03-04 09:56:49 +0700

    Việc 37729: IVT - [CODE] - Chức năng Xử lý khám (HIS.Desktop.Plugins.ExamServiceReqExecute)

--- HIS/Plugins/HIS.Desktop.Plugins.ExamServiceReqExecute/Print/ExamServiceReqExcuteControl__Print___PhieuKhamBenhVaoVien.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.ExamServiceReqExecute/Print/ExamServiceReqExcuteControl__Print___PhieuKhamBenhVaoVien.cs
@@ -145,8 +145,7 @@ namespace HIS.Desktop.Plugins.ExamServiceReqExecute
                 filterViexView.TDL_PATIENT_ID = patient.ID;
                 filterViexView.SERVICE_REQ_ID = this.HisServiceReqView.ID;
                 var sereServViexViews = new BackendAdapter(paramViex).Get<List<V_HIS_SERE_SERV_VIEX>>
-                    (ApiConsumer.HisRequestUriStore.HIS_SERE_SERV_VIEX_GETVIEW, ApiConsumer.ApiConsumers.MosConsumer, filterViexView, HIS.Desktop.Controls.Session.SessionManager.ActionLostToken, paramViex)
-                    ?? new List<V_HIS_SERE_SERV_VIEX>();
+                    (ApiConsumer.HisRequestUriStore.HIS_SERE_SERV_VIEX_GETVIEW, ApiConsumer.ApiConsumers.MosConsumer, filterViexView, HIS.Desktop.Controls.Session.SessionManager.ActionLostToken, paramViex);
                 MPS.Processor.Mps000007.PDO.Mps000007PDO rdo = new MPS.Processor.Mps000007.PDO.Mps000007PDO(
                     patient,
                     patientTypeAlter,
