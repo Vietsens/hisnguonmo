4df15394d382ca86a874290f4ca0e41533a7241c
Nampp <nampp@vietsens.vn>
2026-03-12 17:00:15 +0700

    38327 - Bổ sung tính năng Xuất XML TT12 ký số tại chức năng danh mục Máy cận lâm sàng

---
+++ HIS/Plugins/HIS.Desktop.Plugins.HisMachine/ADO/MachineADO.cs
@@ -0,0 +1,14 @@
+﻿using MOS.EFMODEL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.HisMachine.ADO
+{
+    public class MachineADO : HIS_MACHINE
+    {
+        public bool IsChecked { get; set; }
+    }
+}
--- HIS/Plugins/HIS.Desktop.Plugins.HisMachine/XML/XMLCLSDetailData.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.HisMachine/XML/XMLCLSDetailData.cs
@@ -64,4 +64,51 @@ namespace HIS.Desktop.Plugins.HisMachine.XML
         [XmlElement(Order = 12)]
         public string DEN_NGAY { get; set; }
     }
+
+    [Serializable]
+    public class XMLTT12DetailData
+    {
+        [XmlElement(Order = 1)]
+        public int STT { get; set; }
+
+        [XmlElement(Order = 2)]
+        public XmlCDataSection TEN_TB { get; set; }
+
+        [XmlElement(Order = 3)]
+        public string KY_HIEU { get; set; }
+
+        [XmlElement(Order = 4)]
+        public XmlCDataSection CONGTY_SX { get; set; }
+
+        [XmlElement(Order = 5)]
+        public XmlCDataSection NUOC_SX { get; set; }
+
+        [XmlElement(Order = 6)]
+        public string NAM_SX { get; set; }
+
+        [XmlElement(Order = 7)]
+        public string NAM_SD { get; set; }
+
+        [XmlElement(Order = 8)]
+        public string MA_MAY { get; set; }
+
+        [XmlElement(Order = 9)]
+        public string SO_LUU_HANH { get; set; }
+
+        [XmlElement(Order = 10)]
+        public string HD_TU { get; set; }
+
+        [XmlElement(Order = 11)]
+        public string HD_DEN { get; set; }
+
+        [XmlElement(Order = 12)]
+        public string TU_NGAY { get; set; }
+
+        [XmlElement(Order = 13)]
+        public string DEN_NGAY { get; set; }
+
+        [XmlElement(Order = 14)]
+        public string MA_CSKCB { get; set; }
+    }
+
 }
