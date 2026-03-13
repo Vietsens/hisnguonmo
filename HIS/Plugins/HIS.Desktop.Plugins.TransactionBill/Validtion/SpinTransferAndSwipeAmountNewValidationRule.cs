80f3d969954eb584029dcc1230714ec792aca239
Dang Pro <dangth@vietsens.vn>
2026-03-13 15:57:00 +0700

    Việc 38557: IVT - [DB] - Sửa chức năng thanh toán 1 sổ (HIS.Desktop.Plugins.TransactionBill)

---
+++ HIS/Plugins/HIS.Desktop.Plugins.TransactionBill/Validtion/SpinTransferAndSwipeAmountNewValidationRule.cs
@@ -0,0 +1,44 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.DXErrorProvider;
+using System;
+
+namespace HIS.Desktop.Plugins.TransactionBill.Validtion
+{
+    public class SpinTransferAndSwipeAmountNewValidationRule : ValidationRule
+    {
+        public SpinEdit SpinTransferAmountNew { get; set; }
+        public SpinEdit SpinSwipeAmountNew { get; set; }
+        public Func<long> GetPayFormId { get; set; }
+
+        public override bool Validate(System.Windows.Forms.Control control, object value)
+        {
+            try
+            {
+                if (GetPayFormId == null
+                    || SpinTransferAmountNew == null
+                    || SpinSwipeAmountNew == null)
+                    return true;
+
+                var payFormId = GetPayFormId();
+                if (payFormId != 9) 
+                    return true;
+
+                var ck = SpinTransferAmountNew.Value;
+                var qt = SpinSwipeAmountNew.Value;
+
+                if (ck <= 0 && qt <= 0)
+                {
+                    ErrorText = "Vui lòng nhập số tiền chuyển khoản hoặc số tiền quẹt thẻ.";
+                    ErrorType = ErrorType.Warning;
+                    return false;
+                }
+
+                return true;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+    }
+}
