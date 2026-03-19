e90ede0e0106a1d0de94c17de0bf9de2a5925558
khainq <khainq@vietsens.vn>
2026-03-19 16:20:10 +0700

    thêm valid

---
+++ HIS/Plugins/HIS.Desktop.Plugins.TransactionBillOther/Validation/SpinSwipeAmountValidationRule.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HIS.Desktop.Plugins.TransactionBillOther.Validation
+{
+    class SpinSwipeAmountValidationRule : DevExpress.XtraEditors.DXErrorProvider.ValidationRule
+    {
+        public SpinEdit spinSwipeAmount;
+        public LookUpEdit cboPayForm;
+        public override bool Validate(Control control1, object value)
+        {
+            bool valid = false;
+            try
+            {
+                if (spinSwipeAmount == null)
+                    return valid;
+
+                if (spinSwipeAmount.Value == 0 && cboPayForm?.EditValue != null && Convert.ToInt64(cboPayForm.EditValue) == 9)
+                {
+                    ErrorText = "Số tiền quẹt thẻ phải lớn hơn 0";
+                    ErrorType = DevExpress.XtraEditors.DXErrorProvider.ErrorType.Warning;
+                    return valid;
+                }
+
+                valid = true;
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+            }
+            return valid;
+        }
+    }
+}
