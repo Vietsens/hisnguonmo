3b3fe53ab4d6e9c228d9f63bf1f66f1d715f5588
Nampp <nampp@vietsens.vn>
2026-03-21 18:43:57 +0700

    Bổ sung delegate refresh list

---
+++ HIS/Plugins/HIS.Desktop.Plugins.RefundByTransfer/RefundByTransfer/RefundByTransferBehavior.cs
@@ -0,0 +1,78 @@
+﻿using HIS.Desktop.Common;
+using Inventec.Core;
+using Inventec.Desktop.Core;
+using Inventec.Desktop.Core.Tools;
+using MOS.EFMODEL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.RefundByTransfer.RefundByTransfer
+{
+    class RefundByTransferBehavior : Tool<IDesktopToolContext>, IRefundByTransfer
+    {
+        object[] entity;
+
+        public RefundByTransferBehavior()
+            : base()
+        {
+
+        }
+
+        public RefundByTransferBehavior(CommonParam param, object[] filter)
+            : base()
+        {
+            this.entity = filter;
+        }
+
+        object IRefundByTransfer.Run()
+        {
+            try
+            {
+                Inventec.Desktop.Common.Modules.Module moduleData = null;
+                HIS_TREATMENT treatment = null;
+                HIS_TRANSACTION transaction = null;
+                string bankCode = "";
+                RefeshReference refresh = null;
+                if (entity.GetType() == typeof(object[]))
+                {
+                    if (entity != null && entity.Count() > 0)
+                    {
+                        for (int i = 0; i < entity.Count(); i++)
+                        {
+                            if (entity[i] is Inventec.Desktop.Common.Modules.Module)
+                            {
+                                moduleData = (Inventec.Desktop.Common.Modules.Module)entity[i];
+                            }
+                            else if (entity[i] is HIS_TREATMENT)
+                            {
+                                treatment = (HIS_TREATMENT)entity[i];
+                            }
+                            else if (entity[i] is HIS_TRANSACTION)
+                            {
+                                transaction = (HIS_TRANSACTION)entity[i];
+                            }
+                            else if (entity[i] is string)
+                            {
+                                bankCode = (string)entity[i];
+                            }
+                            else if (entity[i] is HIS.Desktop.Common.RefeshReference)
+                            {
+                                refresh = (HIS.Desktop.Common.RefeshReference)entity[i];
+                            }
+                        }
+                    }
+                }
+
+                return new FormRefundByTransfer(moduleData, bankCode, treatment, transaction, refresh);
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+                return null;
+            }
+        }
+    }
+}
