da4730a468aed2f37a117331f5f6e803388a14a4
huyvu20 <huydvhe170470@fpt.edu.vn>
2026-03-26 17:07:03 +0700

    sửa tên chức năng

--- HIS/Plugins/HIS.Desktop.Plugins.MedicineTypeCreate/MedicineTypeCreate/frmMedicineTypeCreate.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.MedicineTypeCreate/MedicineTypeCreate/frmMedicineTypeCreate.cs
@@ -650,7 +650,7 @@ namespace HIS.Desktop.Plugins.MedicineTypeCreate.MedicineTypeCreate
                 List<AMedicineTypeADO> listADO = new List<AMedicineTypeADO>()
                 {
                     new AMedicineTypeADO { ID = 1,  NAME = "Hóa chất" },
-                    new AMedicineTypeADO { ID = 2,  NAME = "Sản phẩm không phải là thuốc" },
+                    new AMedicineTypeADO { ID = 2,  NAME = "Sản phẩm không phải là thuốc(TPCN, mỹ phẩm,dinh dưỡng,....)" },
                     new AMedicineTypeADO { ID = 3,  NAME = "Thuốc dấu sao *" },
                     new AMedicineTypeADO { ID = 4,  NAME = "Generic" },
                     new AMedicineTypeADO { ID = 5,  NAME = "Vaccine" },
@@ -1587,7 +1587,7 @@ namespace HIS.Desktop.Plugins.MedicineTypeCreate.MedicineTypeCreate
                     arr.Add("Generic");
 
                 if (hIS_MEDICINE_TYPE.IS_FUNCTIONAL_FOOD == 1)
-                    arr.Add("Sản phẩm không phải là thuốc");
+                    arr.Add("Sản phẩm không phải là thuốc(TPCN, mỹ phẩm,dinh dưỡng,....)");
 
                 if (hIS_MEDICINE_TYPE.IS_VITAMIN_A == 1)
                     arr.Add("Vitamin A");
