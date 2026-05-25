6241c3b72ba70a2fce3e03f013c9d1710c3f4111
khainq <khainq@vietsens.vn>
2026-05-25 09:47:39 +0700

    Việc 45573: Trả lại icon cho 3 cái nút : duyệt , hủy duyệt , ds y lệnh cho y lệnh phẫu thuật trong duyệt mổ

--- HIS/Plugins/HIS.Desktop.Plugins.ApprovalSurgery/Properties/Resources.Designer.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.ApprovalSurgery/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace HIS.Desktop.Plugins.ApprovalSurgery.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,76 +57,6 @@ namespace HIS.Desktop.Plugins.ApprovalSurgery.Properties {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Approval {
-            get {
-                object obj = ResourceManager.GetObject("Approval", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap demostration {
-            get {
-                object obj = ResourceManager.GetObject("demostration", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap prohibition {
-            get {
-                object obj = ResourceManager.GetObject("prohibition", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap sound {
-            get {
-                object obj = ResourceManager.GetObject("sound", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap tick {
-            get {
-                object obj = ResourceManager.GetObject("tick", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap UnApproval {
-            get {
-                object obj = ResourceManager.GetObject("UnApproval", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap y_lenh {
-            get {
-                object obj = ResourceManager.GetObject("y-lenh", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
