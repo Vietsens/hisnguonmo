bafe5628b53ae160258b402a7c9c287dd458d498
vuongnd <vuongnd@vietsens.vn>
2026-07-08 14:31:37 +0700

    Việc 50953: [fix bug] Lỗi lấy sai dữ liệu loại máu thành ngày xuất dẫn đến lỗi MOS533

--- HIS/Plugins/HIS.Desktop.Plugins.ExpMestDepaCreate/ADO/MediMateTypeADO.cs
+++ HIS/Plugins/HIS.Desktop.Plugins.ExpMestDepaCreate/ADO/MediMateTypeADO.cs
@@ -147,15 +147,15 @@ namespace HIS.Desktop.Plugins.ExpMestDepaCreate.ADO
                 {
                     this.IsBlood = true;
                     this.IsMedicine = false;
-                    this.SERVICE_ID = blood.ServiceId;
-                    this.MEDI_MATE_TYPE_ID = blood.Id;
+                    this.SERVICE_ID = blood.ServiceId ?? blood.ServiceId.Value;
+                    this.MEDI_MATE_TYPE_ID = blood.Id ?? blood.Id.Value;
                     this.MEDI_MATE_TYPE_CODE = blood.BloodTypeCode;
                     this.MEDI_MATE_TYPE_NAME = blood.BloodTypeName;
                     this.SERVICE_UNIT_NAME = "Túi";
                     this.VOLUME = blood.Volume;
                     this.AVAILABLE_AMOUNT = blood.Amount;
                     this.ExpBlood = new ExpBloodTypeSDO();
-                    this.ExpBlood.BloodTypeId = blood.Id;
+                    this.ExpBlood.BloodTypeId = blood.Id ?? blood.Id.Value;
                 }
             }
             catch (Exception ex)
